8ce852b6117d1b8a705fd74aad5297ebd6b6b95e
murungi <murungikirima@gmail.com>
2017-12-04 08:30:58 +0300

    Add components details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Http,Response } from '@angular/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  constructor(private http: Http){}
+
+  userName="";
+  githubData:any="";
+
+  searchUser(){
+  this.http.get("https://api.github.com/users/" + this.userName + "?access_token="+environment.access_token)
+  .subscribe(
+    (response:Response)=>{
+      const userInfo=response.json();
+      this.githubData=userInfo;
+
+      console.log(userInfo);
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from "@angular/forms";
+import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
 
 
@@ -10,9 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [HttpModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
