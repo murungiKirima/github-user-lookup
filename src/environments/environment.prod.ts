9006380b0b2b64a5b0042d0aca78a5a023207fe9
murungi <murungikirima@gmail.com>
2017-12-04 12:25:41 +0300

    Change  .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
-import { API_KEY } from '../../.env';
-
 export const environment = {
   production: true,
-  access_token: API_KEY
+  access_token: "f4f9bdc7c1ded4ea8ce1ea6bbca001e83cd73f1a"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,8 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-import { API_KEY } from '../../.env';
 
 export const environment = {
   production: false,
-  access_token: API_KEY
+  access_token: "f4f9bdc7c1ded4ea8ce1ea6bbca001e83cd73f1a"
 };
